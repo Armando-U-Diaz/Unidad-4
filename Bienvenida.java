81684c3b8990da41cd327175ba5eec4d837c1133
Diaz ArmandoU <diaz.armando.u@gmail.com>
2025-01-16 21:03:40 -0600

    Create Bienvenida.java

---
+++ Bienvenida.java
@@ -0,0 +1,9 @@
+package programasPrimero;
+public class Bienvenida {
+    // Sengundo metodo sintaxis
+    // modificadorAcceso tipoRetorno identificador (lista de parametros)
+    public void mensajeDespedida (String nombre) {  // (String nombre) esto es un parametro
+         System.out.println(nombre + " estamos apunto de terminar el semestre,\nya eres un programado JUNIOR");
+         //un parametro sirven para pasar datos de un lado al otro
+    }
+}
