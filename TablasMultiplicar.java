977485d48b2813be024cdd26de3324d237539f80
Diaz ArmandoU <diaz.armando.u@gmail.com>
2025-01-16 21:04:43 -0600

    Create TablasMultiplicar.java

---
+++ TablasMultiplicar.java
@@ -0,0 +1,17 @@
+package programasPrimero;
+
+public class TablasMultiplicar {
+    public void multi(String []arm) {
+        byte num, x;
+        int resultado = 0;
+        for (num = 1; num <= 10; num ++) {
+            x = 1;
+            System.out.print("Tabla del: " + num + "\n");
+            for (x = 1; x <= 10; x ++) {
+                resultado = num * x;
+                System.out.println(num + " * " + x + " = " + resultado);
+            }
+            System.out.println("\t");
+        }
+    }
+}
